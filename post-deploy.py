a6da60c2241ae85e826887fd5698951e24937cef
Earl Robinson <earl@krashproof.net>
2016-05-07 16:19:20 -0400

    implemented basic s3 file upload to specified bucket
    fix dev.cfg with valid bucket name
    create foo.txt uplad file

--- post-deploy.py
+++ post-deploy.py
@@ -3,6 +3,8 @@
 ## Get env_name arg from cli, and use it to determine which env to run against ##
 
 import argparse
+import botocore
+import boto3
 
 def get_bucket_name(env):
     """ Get the value of bucket_name from the env config file
@@ -16,7 +18,21 @@ def get_bucket_name(env):
         if "bucketName" in line:
             bucket_name = line.split("'")[1]
             print("Bucket Name is: %s" % bucket_name)
+            return(bucket_name)
             break
+
+def put_bucket_object(bucket_name):
+    """ Put object into S3 bucket, using bucketname from env config file """
+    bucket = s3.Bucket(bucket_name)
+
+#    try:
+#        s3.meta.client.head_bucket(Bucket=bucket)
+#    except botocore.exceptions.ClientError as resp:
+#        error_code = int(resp.error_code['Error']['Code'])
+#        print("Error accessing bucket : %s, got error code %s" % bucket_name, error_code)
+#        exit
+
+    s3.Object(bucket_name, "foo.txt").put(Body=open("./foo.txt", "rb"))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("env", type=str, help="Environment name dev|stage|prod")
@@ -24,11 +40,19 @@ args = parser.parse_args()
 
 if args.env == "dev" or args.env == "stage" or args.env == "prod" :
     #print ("Env is " + args.env)
-    get_bucket_name(args.env)
+    bucket_name = get_bucket_name(args.env)
+    s3 = boto3.resource('s3')
+    put_bucket_object(bucket_name)
 else:
     print ("Env has invalid value: " + args.env)
     parser.print_help()
 
+
+#s3 = boto3.resource('s3')
+#for bucket in s3.buckets.all():
+#    print(bucket.name)
+
+
+
 # print trailing crlf
 print("")
-
